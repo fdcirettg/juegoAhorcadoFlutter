a5f44a00f4d98b6c534b6a734ae8c39d11ed77d8
Federico M. Cirett Galán <111463758+fdcirettg@users.noreply.github.com>
2025-08-28 17:31:52 -0700

    Página de Información con datos
    Página de Información (About) completa

--- informacion_page.dart
+++ informacion_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
 
 class InformacionPage extends StatelessWidget {
   const InformacionPage({super.key});
@@ -9,8 +11,71 @@ class InformacionPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Información'),
       ),
-      body: const Center(
-        child: Text('Esta es la página de Información.'),
+      body: Center(
+        child: Column( 
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.center, 
+            crossAxisAlignment: CrossAxisAlignment.start, 
+                    children: [
+                Text('About', style: Theme.of(context).textTheme.displayLarge),
+              ],
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column( 
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset('./../assets/dart_vader_16bit.png',
+                     width:100,),
+                  ]
+                ),
+                Column(mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Darth Vader', style: Theme.of(context).textTheme.headlineMedium,),
+                      Text('Lord of the Sith'),
+                  ],
+              ),
+              ],
+          ),
+        ],
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              child: const Text('Menú'),
+            ),
+            ListTile(
+              title: const Text('Inicio'),
+              onTap: () {
+                context.go('/');
+              },
+            ),
+            ListTile(
+              title: const Text('Juego'),
+              onTap: () {
+                context.go('/juego');
+              },
+            ),
+            ListTile(
+              title: const Text('Configuración'),
+              onTap: () {
+                context.go('/configuracion');
+              },
+            ),
+            ListTile(
+              title: const Text('Información'),
+              onTap: () {
+                context.go('/informacion');
+              }),
+          ],
+        ),
       ),
     );
   }
