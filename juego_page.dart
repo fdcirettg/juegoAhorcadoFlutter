2900eb5b3cce28030acbeca862e95135bceb6cac
Federico M. Cirett Galán <111463758+fdcirettg@users.noreply.github.com>
2025-09-01 18:56:28 -0700

    Update juego_page.dart
    Place holders para Abcedario, imagen del juego y para palabra a adivinar

--- juego_page.dart
+++ juego_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'settings_controller.dart';
 
 class JuegoPage extends StatefulWidget {
-  const JuegoPage({Key? key}) : super(key: key);
-
+  JuegoPage({super.key});
+  final SettingsController settingsController = Get.find(); 
   @override
+  // ignore: library_private_types_in_public_api
   _JuegoPageState createState() => _JuegoPageState();
 }
 
@@ -14,8 +17,38 @@ class _JuegoPageState extends State<JuegoPage> {
       appBar: AppBar(
         title: const Text('Juego'),
       ),
-      body: const Center(
-        child: Text('This is the Juego page'),
+      body: Center(
+          child: Column(children: [
+            Row( mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('ABCD...')
+              ],
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height:300,
+                  width:600,
+                  child: Stack(
+                    children: <Widget>[
+                      widget.settingsController.showTemaSelectedBackground(300),
+                      Positioned(
+                        top: widget.settingsController.getTemaTop(6),
+                        left: widget.settingsController.getTemaLeft(6),
+                        child:  Container(width:150,
+                          child: widget.settingsController.showAhorcado(6,350),)
+                          ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Row( mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('_ _ _ _ _'),
+              ],
+            ),
+          ],),     
       ),
     );
   }
