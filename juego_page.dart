b5f8d1ec58475e1110bc24ab758110e4708b503b
Federico M. Cirett Galán <111463758+fdcirettg@users.noreply.github.com>
2025-09-02 19:04:56 -0700

    Update juego_page.dart
    Agregamos el generador de palabras ocultas, leyendo archivo de texto con la lista de palabras

--- juego_page.dart
+++ juego_page.dart
@@ -1,54 +1,111 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'settings_controller.dart';
 
 class JuegoPage extends StatefulWidget {
   JuegoPage({super.key});
-  final SettingsController settingsController = Get.find(); 
+  final SettingsController settingsController = Get.find();
   @override
   // ignore: library_private_types_in_public_api
   _JuegoPageState createState() => _JuegoPageState();
 }
 
 class _JuegoPageState extends State<JuegoPage> {
+  String archivoPalabras = 'assets/palabras.txt';
+  List<String> palabras = [];
+  String _palabra = "";
+  String _espacios = "";
+  int idxpalabra = 0;
+  int _intentos = 0;
+  final Random _random = Random();
+
+  @override
+  void initState() {
+    super.initState();
+    _cargaPalabras();
+  }
+
+  Future<void> _cargaPalabras() async {
+    final String contenido = await rootBundle.loadString(archivoPalabras);
+    setState(() {
+      palabras = contenido.split('\n');
+    });
+  }
+
+  void _setEspacios() {
+    setState(() {
+      if (palabras.isNotEmpty) {
+        _palabra = palabras[idxpalabra];
+        _espacios = "_ " * _palabra.length;
+      } else {
+        _palabra = "";
+        _espacios = "";
+      }
+    });
+  }
+
+  void _nuevoJuego() {
+    setState(() {});
+    idxpalabra = _random.nextInt(palabras.length);
+    _intentos = 0;
+    _setEspacios();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Juego'),
-      ),
+      appBar: AppBar(title: const Text('Juego')),
       body: Center(
-          child: Column(children: [
-            Row( mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('ABCD...')
-              ],
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [Text('ABCD...')],
             ),
-            Row(mainAxisAlignment: MainAxisAlignment.center,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 SizedBox(
-                  height:300,
-                  width:600,
+                  height: 300,
+                  width: 600,
                   child: Stack(
                     children: <Widget>[
                       widget.settingsController.showTemaSelectedBackground(300),
                       Positioned(
-                        top: widget.settingsController.getTemaTop(6),
-                        left: widget.settingsController.getTemaLeft(6),
-                        child:  SizedBox(width:150,
-                          child: widget.settingsController.showAhorcado(6,350),)
-                          ),
+                        top: widget.settingsController.getTemaTop(_intentos),
+                        left: widget.settingsController.getTemaLeft(_intentos),
+                        child: SizedBox(
+                          width: 150,
+                          child: widget.settingsController.showAhorcado(_intentos, 350),
+                        ),
+                      ),
                     ],
                   ),
                 ),
               ],
             ),
-            Row( mainAxisAlignment: MainAxisAlignment.center,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text('_ _ _ _ _'),
+                //Text('_ _ _ _ _')
+                Text(_espacios),
+                Text(_palabra),
               ],
             ),
-          ],),     
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: _nuevoJuego,
+                  child: const Text('Nuevo Juego'),
+                ), 
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
