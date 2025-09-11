31a0e6995d4521466ebe4d02dced68657fac5551
Federico M. Cirett Galán <111463758+fdcirettg@users.noreply.github.com>
2025-09-10 19:12:09 -0700

    Update informacion_page.dart
    El código de esta página había sido sobreescrito por otra página

--- informacion_page.dart
+++ informacion_page.dart
@@ -1,87 +1,79 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'settings_controller.dart';
+import 'package:go_router/go_router.dart';
 
-List<String> imagenes = ['./../assets/base/background.png','./../assets/futurista/background.png'];
 
-class ConfiguracionPage extends StatefulWidget {
-  ConfiguracionPage({super.key});
-  final SettingsController settingsController = Get.find(); // Retrieve the controller
-  @override
-  State<ConfiguracionPage> createState() => _ConfiguracionPageState();
-}
+class InformacionPage extends StatelessWidget {
+    const InformacionPage({super.key});
 
-class _ConfiguracionPageState extends State<ConfiguracionPage> {
-  // Initialize _temaSeleccionado with a default value
-  late TemaJuego? _temaSeleccionado; // Use late to initialize in initState
-
-  @override
-  void initState() {
-    super.initState();
-    _temaSeleccionado = TemaJuego.values[widget.settingsController.getTema()];
-  }
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Configuración'),
+        title: const Text('Información'),
       ),
       body: Center(
         child: Column( 
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Row(mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                
-                Text('Seleccione el tema deseado', style: Theme.of(context).textTheme.displayMedium),
-              ],
-            ),
             Row(mainAxisAlignment: MainAxisAlignment.center, 
-              children: [DropdownButton<TemaJuego>(
-                  value: _temaSeleccionado,
-                  onChanged: (TemaJuego? nuevoValor) {
-                    setState(() {
-                      _temaSeleccionado = nuevoValor;
-                    });
-                  },
-                  items:  <DropdownMenuItem<TemaJuego>>[
-                    DropdownMenuItem<TemaJuego>(
-                      value: TemaJuego.base,
-                      child: Text('Base'),
-                    ),
-                    DropdownMenuItem<TemaJuego>(
-                      value: TemaJuego.futurista,
-                      child: Text('Futurista'),
-                    ),
-                    DropdownMenuItem<TemaJuego>(
-                      value: TemaJuego.terror,
-                      child: Text('Terror'),
-                    ),
-                  ],
-                ),
-],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                //Image.asset(imagenes[_temaSeleccionado!.index],
-                //height: 100,),
-                widget.settingsController.showTemaBackground(100),
-              ]
+            crossAxisAlignment: CrossAxisAlignment.start, 
+                    children: [
+                Text('About', style: Theme.of(context).textTheme.displayLarge),
+              ],
             ),
             Row(mainAxisAlignment: MainAxisAlignment.center,
               children: [
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  // Add functionality to save preference
-                  widget.settingsController.actualizaTema( _temaSeleccionado!.index);
-
-                },
-                child: Text('Guardar preferencia'),
-              )
-            ],)
+                Column( 
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset('./../assets/dart_vader_16bit.png',
+                     width:100,),
+                  ]
+                ),
+                Column(mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Darth Vader', style: Theme.of(context).textTheme.headlineMedium,),
+                      Text('Lord of the Sith'),
+                  ],
+              ),
+              ],
+          ),
+        ],
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              child: const Text('Menú'),
+            ),
+            ListTile(
+              title: const Text('Inicio'),
+              onTap: () {
+                context.go('/');
+              },
+            ),
+            ListTile(
+              title: const Text('Juego'),
+              onTap: () {
+                context.go('/juego');
+              },
+            ),
+            ListTile(
+              title: const Text('Configuración'),
+              onTap: () {
+                context.go('/configuracion');
+              },
+            ),
+            ListTile(
+              title: const Text('Información'),
+              onTap: () {
+                context.go('/informacion');
+              }),
           ],
         ),
       ),
