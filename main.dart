1a9b3bcb14de6c3c55e8edd2671169a58cb80860
Federico M. Cirett Galán <111463758+fdcirettg@users.noreply.github.com>
2025-09-10 19:12:40 -0700

    Update main.dart
    Agregamos imagen de fondo y botón de inicio

--- main.dart
+++ main.dart
@@ -85,29 +85,36 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('Bienvenidos/as al juego del Ahorcado'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+        child: Container(
+            decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("game_background_1200.jpg"),
+              //fit: BoxFit.cover,
+              fit: BoxFit.fitWidth,
             ),
-          ],
-        ),
+          ),
+          child: Column(
+            children: [
+              SizedBox(height: 20),
+              Row(mainAxisAlignment: MainAxisAlignment.center,
+                children: [Text('Bienvenido al juego del ahorcado'),],
+              ),
+          
+              SizedBox(height: 80),
+              Row(mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      context.go('/juego');
+                    },
+                    child: const Text('Iniciar Juego'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+           ),
       ),
       drawer: Drawer(
         child: ListView(
